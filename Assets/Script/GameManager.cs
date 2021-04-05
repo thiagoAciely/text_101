fa55dc4dc7b085fb04ee4c66c3e9d899efb1f0a8
Thiago Aciely <taciely@gmail.com>
2021-04-04 21:50:16 -0300

    Initial Comit

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private State _startState;
+    [SerializeField] 
+    private TMP_Text _storyText;
+    [SerializeField]
+    private Button[] _choiceButtons;
+
+    private State _currentState;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StoryUpdate(_startState);
+    }
+
+    void StoryUpdate (State nextState)
+	{
+        Debug.Log(nextState.GetTitle());
+        _currentState = nextState;
+        _storyText.text = _currentState.GetStory();
+        Debug.Log(nextState.GetTitle());
+        SetButtons(nextState.GetNext());
+    }
+
+    void SetButtons(State[] choices)
+	{
+        foreach (Button b in _choiceButtons)
+		{
+            b.gameObject.SetActive(false);
+		}
+
+        for (int i = 0; i < choices.Length; ++i)
+		{
+            _choiceButtons[i].gameObject.SetActive(true);
+            _choiceButtons[i].GetComponentInChildren<TMP_Text>().text = choices[i].GetTitle();
+            
+            int index = i;
+            _choiceButtons[i].onClick.AddListener(() => StoryUpdate(choices[index]));
+        }
+	}
+}
---
+++ Assets/Script/State.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Scriptable/State")]
+public class State : ScriptableObject
+{
+	[TextArea(15, 20)]
+	[SerializeField]
+	private string stateText;
+	[SerializeField]
+	private string stateTitle;
+
+	[SerializeField]
+	private State[] nextStates;
+
+	public string GetStory()
+	{
+		return this.stateText;
+	}
+
+	public string GetTitle()
+	{
+		return this.stateTitle;
+	}
+
+	public State[] GetNext()
+	{
+		return this.nextStates;
+	}
+}
